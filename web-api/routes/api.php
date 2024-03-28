7642db5e973529f077934b3620de25ea97afa606
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-03-28 20:49:39 +0200

    Add CRUD Swagger Category

--- web-api/routes/api.php
+++ web-api/routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Api\CategoriesController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/categories', [CategoriesController::class, 'getList']);
+Route::get('/categories/{id}', [CategoriesController::class, 'getById']);
+Route::post('/categories/create', [CategoriesController::class, 'create']);
+Route::delete("/categories/{id}", [CategoriesController::class, "delete"]);
+Route::post("/categories/edit/{id}", [CategoriesController::class, "edit"]);
