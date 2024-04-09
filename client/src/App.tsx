b76fbf2e17f8f0325a56f0d42db0692afbbd60cb
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-04-09 20:48:39 +0300

    Add Admin layout and data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css'
+// import './App.css'
+import CategoryListPage from "./components/categories/list/CategoryListPage.tsx";
+import {Route, Routes} from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import AdminLayout from "./components/containers/admin/AdminLayout.tsx";
 
 const App = () => {
 
-  return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<AdminLayout/>}>
+                    <Route index element={<CategoryListPage/>}/>
+                    {/*<Route path="users" element={<UsersTasks />} />*/}
+                    {/*<Route path="tasks" element={<TasksPage />} />*/}
+                </Route>
+
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App
